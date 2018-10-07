22ee87bda34fe008a616f04218d2d6192afc6bc0
Charlie Barrett <37375131+cbarrett32@users.noreply.github.com>
2018-10-07 18:19:11 -0400

    Add files via upload

---
+++ Our Scripts/Sounds/AwarenessSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwarenessSound : MonoBehaviour {
+	AudioSource awarenessMeter;
+
+	// Use this for initialization
+	void Start () {
+		awarenessMeter = this.gameObject.GetComponent<AudioSource> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void playSound()
+	{
+		awarenessMeter.Play ();
+	}
+	public void stopSound()
+	{
+		awarenessMeter.Stop ();
+	}
+}
---
+++ Our Scripts/Sounds/FrogDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogDeath : MonoBehaviour {
+	AudioSource frogDeath;
+
+	// Use this for initialization
+	void Start () {
+		frogDeath = this.gameObject.GetComponent<AudioSource> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void playSound()
+	{
+		frogDeath.Play ();
+	}
+}
