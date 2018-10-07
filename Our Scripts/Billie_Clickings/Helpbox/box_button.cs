a34c00df74525f366c407781ed21cf8c150117e0
Charlie Barrett <37375131+cbarrett32@users.noreply.github.com>
2018-10-07 18:22:51 -0400

    Add files via upload

---
+++ Our Scripts/Billie_Clickings/Helpbox/box_button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box_button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void click_and_close1(string variable) {
+		// once closed, will never show up again
+		PlayerPrefs.SetInt (variable, 1);
+		// close the panel
+		Destroy(transform.parent.gameObject);
+	}
+}
