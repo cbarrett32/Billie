070096cdcdb166030962f983ab7b49deea51e3cc
Charlie Barrett <37375131+cbarrett32@users.noreply.github.com>
2018-10-07 18:16:28 -0400

    Add files via upload

---
+++ Our Scripts/AlertedSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertedSound : MonoBehaviour {
+	AudioSource bugleCall;
+
+	// Use this for initialization
+	void Start () {
+		bugleCall = this.gameObject.GetComponent<AudioSource> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void playSound()
+	{
+		bugleCall.Play ();
+	}
+}
